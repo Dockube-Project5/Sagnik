d8223f3e2cf79cac4a7e285a908abdd46c61fab3
Sagnik Roy <sagnikr1999@gmail.com>
2021-08-01 16:11:31 +0530

    updated readme

--- app.py
+++ app.py
@@ -4,7 +4,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return jsonify({"Time of Call": time.time()})
+    return jsonify(
+        {"Time of Call": time.strftime("%H:%M:%S", time.localtime()),
+        "Name" : "Sagnik",
+        "Comment" : "I am learning Docker and Kubernetes",
+        "Team" : "DockKube"
+    })
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
